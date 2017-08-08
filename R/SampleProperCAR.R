f5386186551a3c2a909f8e78b83ee51d1c5eb10f
ajharry <harry.april@gmail.com>
2017-08-08 16:20:19 -0400

    updates to improve single sims

--- R/SampleProperCAR.R
+++ R/SampleProperCAR.R
@@ -1,4 +1,4 @@
-#' @title sample car
+#' @title sample car2
 #' @description sample car
 #' @param tau2 safaf
 #' @return sample
@@ -28,39 +28,44 @@
 #SigmaInv = tau^2 * Dw %*% (I - diag(rho, Wstd )
 
 sampleCAR <- function(condDiff, coord = expand.grid(x=1:10, y=1:10),
-                      pattern = ifelse((coords$x %in% 3:8 & coords$y %in% 3:8), 2, 1),
+                      pattern = rep(1, 100),
                       sig2, tau2, rho = .9999, nrep=100, save = F, randomSeed = 1,
                       neighbor.type = "radius", radius = 1){
 
-            set.seed(randomSeed)
+  set.seed(randomSeed)
 
-            W <- adj.grid(coord, sample= factor(rep(1, nrow(coord))),
-                          type = neighbor.type, radius = radius)
-            n <- nrow(W)
-            m <- rowSums(W)
-            Wstd <- diag(1/m)%*%W
-            Dw <- diag(m)
+  pattern <- factor(pattern)
 
-            phisamp <- matrix(nrow = nrep, ncol = n)
-            tau_1m <- sqrt(tau2)					      # Spatial SD
-            SigmaInv <-as.spam(diag(m,n) - rho * W)
-            Sigma <-  tau2 * solve(SigmaInv)			# Covariance of phis
+  W <- adj.grid(coord, sample= factor(rep(1, nrow(coord))),
+                type = neighbor.type, radius = radius)
+  n <- nrow(W)
+  m <- rowSums(W)
+  #Wstd <- diag(1/m)%*%W
+  #Dw <- diag(m)
 
-            for(i in 1:nrep){
-              # Spatial Random Effects +  # Measurement error + # Condition Effect
-              sp <- c(rmvnorm(1,sigma=Sigma))
-              phisamp[i,] <- (sp - mean(sp)) + rnorm(n, mean = 0, sd = sqrt(sig2))  + ifelse(pattern == 2, condDiff, 0)
-            }
+  phisamp <- matrix(nrow = nrep, ncol = n)
+  tau_1m <- sqrt(tau2)					      # Spatial SD
+  SigmaInv <-as.spam(diag(m,n) - rho * W)
+  Sigma <-  tau2 * solve(SigmaInv)			# Covariance of phis
+
+  for(i in 1:nrep){
+    # Spatial Random Effects +  # Measurement error + # Condition Effect
+    sp <- c(rmvnorm(1,sigma=Sigma))
+    for(q in levels(pattern)){
+      sp[pattern == q] <- sp[pattern == q] - mean(sp[pattern == q])
+    }
+    phisamp[i,] <- sp + rnorm(n, mean = 0, sd = sqrt(sig2))  + ifelse(pattern == 2, condDiff, 0)
+  }
 
 
 
-            if(save){
-              assign(paste("phi_r", rho, "_t", tau2,"_s",sig2,"_a",condDiff, sep=""), phisamp)
-              save(list = paste("phi_r", rho, "_t", tau2,"_s",sig2,"_a",condDiff, sep=""),
-                 file = paste("phi_r", rho, "_t", tau2,"_s",sig2,"_a",condDiff, ".rdata", sep="") )
-            }
+  if(save){
+    assign(paste("phi_r", rho, "_t", tau2,"_s",sig2,"_a",condDiff, sep=""), phisamp)
+    save(list = paste("phi_r", rho, "_t", tau2,"_s",sig2,"_a",condDiff, sep=""),
+         file = paste("phi_r", rho, "_t", tau2,"_s",sig2,"_a",condDiff, ".rdata", sep="") )
+  }
 
-            return(phisamp)
+  return(phisamp)
 }
 
 
